6c06043667d5a10e90e49028794a85d715f1bde8
William Bell <wtb2804@gmail.com>
2020-05-14 00:10:42 -0400

    Inital logic setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import PhotoCard from "./components/PhotoCard";
+import Information from "./components/Information";
+
 
 function App() {
+
+  const {nasa, setNasa } = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=hzbjykgYdMZ2MHqBc2tp19mNzF9sg5MwDbGoyo3p`)
+      .then(res => {
+        setNasa(res.data)
+        console.log(nasa)
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+      })
+    }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header title={nasa} />
+      {/* props needed, title={title} */}
+      <PhotoCard image={nasa}/>
+       {/* props needed, image={hdurl} */}
+       <Information explanation={nasa}/>
+       {/* props needed, explanation={explanation} */}
     </div>
   );
 }
 
 export default App;
+
+
+// key hzbjykgYdMZ2MHqBc2tp19mNzF9sg5MwDbGoyo3p
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header(props) {
+    return (
+        <div>
+            <h1>
+                NASA Photo of the Day
+            </h1>
+            <h2>
+                {/* nasa.title */}
+            </h2>
+        </div>
+    )
+}
+
+export default Header
